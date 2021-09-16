809f95a39e1d2af2a719ee967d9f28d3f7b7322c
Juhwan Song <jsong785@gmail.com>
2021-09-15 23:16:57 -0600

    first commit

---
+++ slice/test_slice.go
@@ -0,0 +1,13 @@
+package slice
+
+import(
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestSlice(t *testing>T) {
+
+    data := make([]int, 0, 5);
+    data = append(data, 1)
+}
+
