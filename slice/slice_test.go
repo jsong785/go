327bb9bbae1772f13bff029ad5b93580841affbf
Juhwan Song <jsong785@gmail.com>
2021-09-15 23:25:49 -0600

    go format

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -1,26 +1,25 @@
 package slice
 
-import(
-    "testing"
-    "github.com/stretchr/testify/assert"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestSliceIsJustAView(t *testing.T) {
 
-    data := make([]int, 0, 5);
-    data = append(data, 1)
-    assert.Equal(t, data, []int{1})
+	data := make([]int, 0, 5)
+	data = append(data, 1)
+	assert.Equal(t, data, []int{1})
 
-    func(d []int){
-        d = append(d, 2);
-        assert.Equal(t, d, []int{1, 2})
-    }(data)
-    assert.Equal(t, data, []int{1})
+	func(d []int) {
+		d = append(d, 2)
+		assert.Equal(t, d, []int{1, 2})
+	}(data)
+	assert.Equal(t, data, []int{1})
 
-    func(d *[]int){
-        *d = append(*d, 2);
-        assert.Equal(t, *d, []int{1, 2})
-    }(&data)
-    assert.Equal(t, data, []int{1, 2})
+	func(d *[]int) {
+		*d = append(*d, 2)
+		assert.Equal(t, *d, []int{1, 2})
+	}(&data)
+	assert.Equal(t, data, []int{1, 2})
 }
-
