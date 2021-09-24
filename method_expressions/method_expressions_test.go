314494eb39b1029229433d8428b431750c808958
Juhwan Song <jsong785@gmail.com>
2021-09-24 02:30:02 -0600

    go fmt

--- method_expressions/method_expressions_test.go
+++ method_expressions/method_expressions_test.go
@@ -1,32 +1,31 @@
 package method_expressions
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 type Data struct {
-    val int
+	val int
 }
 
-func (d *Data)  Add(val int) {
-    d.val += val
+func (d *Data) Add(val int) {
+	d.val += val
 }
 
 func (d *Data) Multiply(val int) {
-    d.val *= val
+	d.val *= val
 }
 
 func TestMethodExpressions(t *testing.T) {
 
-    data := Data{ val: 1 }
-    for _, bleh := range [...]func(*Data, int){ (*Data).Add, (*Data).Multiply } {
-        bleh(&data, 2)
-    }
-    assert.Equal(t, 6, data.val)
+	data := Data{val: 1}
+	for _, bleh := range [...]func(*Data, int){(*Data).Add, (*Data).Multiply} {
+		bleh(&data, 2)
+	}
+	assert.Equal(t, 6, data.val)
 
-    addFunc := data.Add
-    addFunc(10)
-    assert.Equal(t, 16, data.val)
+	addFunc := data.Add
+	addFunc(10)
+	assert.Equal(t, 16, data.val)
 }
-
